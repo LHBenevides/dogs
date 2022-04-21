8917f5ea37c4758e1f11c023e4ef5aa7c64c28c0
Luiz Benevides <luiz_h.b@outlook.com>
2022-04-21 00:28:07 -0300

    criacao FeedPhotos

--- src/Components/Feed/FeedPhotos.js
+++ src/Components/Feed/FeedPhotos.js
@@ -6,7 +6,7 @@ import Error from '../Helper/Error'
 import Loading from '../Helper/Loading'
 import styles from './FeedPhotos.module.css'
 
-const FeedPhotos = () => {
+const FeedPhotos = ({setModalPhoto}) => {
 
   const { data, loading, error, request } = useFetch();
 
@@ -29,6 +29,7 @@ const FeedPhotos = () => {
       <FeedPhotosItem 
       key={photo.id} 
       photo={photo} 
+      setModalPhoto={setModalPhoto}
       />
       ))}
     </ul>
