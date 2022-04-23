f82a6cbfdde0fca83d92786a76fa9ead0060149c
Luiz Benevides <luiz_h.b@outlook.com>
2022-04-23 00:32:15 -0300

    bug fix main

--- src/App.js
+++ src/App.js
@@ -16,23 +16,25 @@ import NotFound from './Components/NotFound';
 
 function App() {
   return (
-    <div>
+    <div className="app">
       <BrowserRouter>
         <UserStorage>
-          <Header />
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='login/*' element={<Login />} />
-            <Route path='conta/*' element={
-              <ProtectedRoute>
-                <User />
-              </ProtectedRoute>
-            }
-            />
-            <Route path='foto/:id' element={<Photo />} />
-            <Route path='perfil/:user' element={<UserProfile />} />
-            <Route path='*' element={<NotFound />} />
-          </Routes>
+          <main className="appBody">
+            <Header />
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='login/*' element={<Login />} />
+              <Route path='conta/*' element={
+                <ProtectedRoute>
+                  <User />
+                </ProtectedRoute>
+              }
+              />
+              <Route path='foto/:id' element={<Photo />} />
+              <Route path='perfil/:user' element={<UserProfile />} />
+              <Route path='*' element={<NotFound />} />
+            </Routes>
+          </main>
           <Footer />
         </UserStorage>
       </BrowserRouter>
